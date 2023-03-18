80b018fc4d89934a6e5e04aad4ecb7f120acdf56
seifollah <seifollah2018@gmail.com>
2023-03-19 01:24:28 +0330

    courses-Infos

--- src/Page/Category/Category.js
+++ src/Page/Category/Category.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../Commponent/Header/Header'
 import Footer from '../../Commponent/Footer/Footer'
+import Cart from '../../Commponent/Cart/Cart'
 export default function Category() {
   return (
     <>
       <Header />
 
+      <Cart />
       <Footer />
     </>
   )
